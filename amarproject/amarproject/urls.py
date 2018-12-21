e250cb5c3728c72b0ad789fba575a377e9e9cabe
bhagyo <prachinmon1995@gmail.com>
2018-12-22 02:40:09 +0600

    clean code of creating urls in an app

--- amarproject/amarproject/urls.py
+++ amarproject/amarproject/urls.py
@@ -1,28 +1,10 @@
-"""amarproject URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-from blog_post.views import home
+from django.urls import path, include
 from student.views import page1
-from blog_post.views import post_list, single_post
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home, name='home'),
+    path('blog/', include('blog_post.urls'), name='home'),
     path('page1/', page1, name='page1'),
-    path('post-list/', post_list, name='post-list'),
-    path('single-post/<post_id>/', single_post, name='single-post'),
 ]
---
+++ amarproject/blog_post/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('home/', views.home, name='home'),
+    path('post-list/', views.post_list, name='post-list'),
+    path('single-post/<post_id>/', views.single_post, name='single-post')
+]
