d16b4f4ca9b270a2e2dd15a5ed0ee3c880b20cc8
bhagyo <prachinmon1995@gmail.com>
2018-12-24 14:57:22 +0600

    added divisions,district and filter mode

--- amarproject/information/urls.py
+++ amarproject/information/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('divisions/', views.division_list, name='divisions'),
     path('districts/', views.district_list, name='districts'),
+    path('districts/<int:div_id>', views.dists_of_division, name='dists_of_division'),
 ]
--- amarproject/information/views.py
+++ amarproject/information/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 # Create your views here.
 from .models import Districts
+from .models import Divisions
 
 
 def district_list(request):
     districts = Districts.objects.all()
     context = {'districts': districts}
     return render(request, 'information/districts.html', context)
+
+
+def division_list(request):
+    divisions = Divisions.objects.all()
+    context = {'divisions': divisions}
+    return render(request, 'information/division_list.html', context)
+
+
+def dists_of_division(request, div_id):
+    divisions = Divisions.objects.get(pk=div_id)
+    districts = Districts.objects.filter(division=divisions)
+    context = {'divisions': divisions, 'districts': districts}
+    return render(request, 'information/dists_of_division.html', context)
